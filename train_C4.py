dc33458273f02deec999415273da4afcc359e594
Blake Bordelon <bbordelon@holylogin02.rc.fas.harvard.edu>
2024-05-02 14:11:20 -0400

    add pos encoding, data grouping for C4 for longer ctx, qk-layernorm, switch to 1/L scaling

--- train_C4.py
+++ train_C4.py
@@ -16,6 +16,7 @@ import os
 from datasets import load_dataset
 from transformers import AutoTokenizer
 from transformers import GPT2TokenizerFast
+from itertools import chain
 
 
 
@@ -27,14 +28,14 @@ parser = argparse.ArgumentParser(description=''
 parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
 parser.add_argument('--mom', default=0.0, type=float, help='momentum')
 parser.add_argument('--batch_size', type=int, default=256)
-parser.add_argument('--max_len', type=int, default=128)
+parser.add_argument('--max_len', type=int, default=256)
 parser.add_argument('--arch', type=str, default='LLM')
 parser.add_argument('--dataset', type=str,default = 'C4')
 parser.add_argument('--optimizer', default='adam')
 parser.add_argument('--width', type=int, default = 32)
 parser.add_argument('--heads', type=int, default = 4)
 parser.add_argument('--patch_size',type=int, default=4)
-parser.add_argument('--depth', type=int, default = 2)
+parser.add_argument('--depth', type=int, default = 4)
 parser.add_argument('--beta', type=float, default=4.0,
                          help='scaling factor for the residual branch. To use together with res_scaling parameter')
 parser.add_argument('--gamma_zero', type=float, default=0.1,
@@ -72,7 +73,6 @@ else:
     adam = False
     
 
-    
 
 save_dir = '/n/holyscratch01/pehlevan_lab/Users/bbordelon/bbordelon/Learn_gates/C4_LLM'
 
@@ -86,33 +86,54 @@ def get_run_name(args):
 MAX_LEN = args.max_len # length of the context window (consider making this longer)
 BATCH_SIZE = args.batch_size # size of the batches
 
-ds = load_dataset('c4', 'en', split='train', streaming = True)
+
+ds = load_dataset("allenai/c4", 'en', streaming = True)['train']
 #ds = ds.with_format('jax')
-shuff_ds = ds.shuffle(seed = 0, buffer_size = 20000)
+shuff_ds = ds.shuffle(seed = 0, buffer_size = 10000)
 
 tokenizer = GPT2TokenizerFast.from_pretrained("openai-community/gpt2")
 tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
 VOCAB_SIZE = len(tokenizer)
 
+
 # encoder function: GPT byte encoding
 def encode(examples):
-    return tokenizer(examples['text'], truncation=True, padding='max_length', max_length = MAX_LEN)
+    #return tokenizer(examples['text'], truncation=True, padding='max_length', max_length = MAX_LEN)
+    return tokenizer(examples['text'])
 
-dataset = shuff_ds.map(encode, batched=True, batch_size = BATCH_SIZE, remove_columns=["timestamp", "url"])
+# create groups of text of appropriate size
+def group_texts(examples):
+    # Concatenate all texts.
+    concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the small remainder, and if the total_length < max_seq_length  we exclude this batch and return an empty dict.
+    # We could add padding if the model supported it instead of this drop, you can customize this part to your needs.
+    if total_length >= MAX_LEN:        
+        total_length = (total_length // MAX_LEN) * MAX_LEN
+    # Split by chunks of max_len.
+    result = {
+                k: [t[i : i + MAX_LEN] for i in range(0, total_length, MAX_LEN)]
+                for k, t in concatenated_examples.items()
+    }
+    return result
 
+dataset = shuff_ds.map(encode, batched= True, remove_columns=["timestamp", "url"])
+
+#dataset_grouped = dataset.map(group_texts, batched = True, batch_size = BATCH_SIZE)
+dataset_grouped = dataset.map(group_texts, batched = True)
 
 import jax.numpy as jnp
 from torch.utils.data import DataLoader
-from transformers import DataCollatorForLanguageModeling
 import numpy as np
 
 # collate without mlm
 
-collate_fn = lambda x: np.array([xi["input_ids"] for xi in x])
+collate_fn = lambda x: [ xi["input_ids"] for xi in x ] 
 
-# get the dataloader for this dataset
-dataloader = DataLoader(dataset, batch_size = BATCH_SIZE, collate_fn = collate_fn)
+#dataloader = DataLoader(dataset, batch_size = BATCH_SIZE, collate_fn = collate_fn)
+dataloader = DataLoader(dataset_grouped, batch_size = BATCH_SIZE, collate_fn = collate_fn)
+
 
 class Causal_Attention(nn.Module):
 
@@ -127,9 +148,9 @@ class Causal_Attention(nn.Module):
         kif_qk = nn.initializers.normal(stddev = self.dim**(self.c - 0.5) ) # possibly needs to be scaled with N
         kif_v =  nn.initializers.normal(stddev = 1.0 ) # O_N(1) entries
         # computes key, query, value
-        self.qk_layer = nn.Dense(features = 2 * self.heads * self.dim, kernel_init = kif_qk)
-        self.v_layer = nn.Dense(features = self.heads * self.dim, kernel_init = kif_v)
-        self.out_layer = nn.Dense(features = self.heads * self.dim, kernel_init = kif_v)
+        self.qk_layer = nn.Dense(features = 2 * self.heads * self.dim, kernel_init = kif_qk, use_bias = False)
+        self.v_layer = nn.Dense(features = self.heads * self.dim, kernel_init = kif_v, use_bias = False)
+        self.out_layer = nn.Dense(features = self.heads * self.dim, kernel_init = kif_v, use_bias = False)
         self.q_norm = nn.LayerNorm()
         self.k_norm = nn.LayerNorm()
         return
@@ -154,29 +175,30 @@ class Causal_Attention(nn.Module):
         out = rearrange(out, 'b h l d -> b l (h d)') 
         out = self.out_layer(out) / jnp.sqrt( out.shape[-1] )
         return out
+
     
 class MLP_Block(nn.Module):
 
     features: int
-    
     @nn.compact
     def __call__(self,x):
         N = self.features
         kif = nn.initializers.normal(stddev = 1.0) # O_N(1) entries
-        h = nn.Dense(features = N, kernel_init = kif)(x) / jnp.sqrt(N)
-        h = nn.relu(h)
-        h = nn.Dense(features = N, kernel_init = kif)(x) / jnp.sqrt(N)
+        h = nn.Dense(features = N, kernel_init = kif, use_bias = False)(x) / jnp.sqrt(N)
+        h = nn.gelu(h)
+        h = nn.Dense(features = N, kernel_init = kif, use_bias = False)(h) / jnp.sqrt(N)
         return h
-    
 
+    
 class PositionalEncoding(nn.Module):
     d_model : int         # Hidden dimensionality of the input.
+    scale: jnp.float32   # initial stddev of entries
     max_len : int = MAX_LEN  # Maximum length of a sequence to expect.
     
     def setup(self):
         # Create matrix of [SeqLen, HiddenDim] representing the positional encoding for max_len inputs
         self.pos_embedding = self.param('pos_embedding', 
-                                        nn.initializers.normal(stddev=1.0), 
+                                        nn.initializers.normal(stddev=self.scale), 
                                         (1, 1+self.max_len, self.d_model))
 
     def __call__(self, x, train=True):
@@ -202,10 +224,10 @@ class Transformer(nn.Module):
         kif0 = nn.initializers.normal(stddev = 0.0 )
         kif = nn.initializers.normal(stddev = 1.0) # O_N(1) entries
         kif_last = nn.initializers.normal(stddev = L**(0.5 * (1-self.adam_scale)) * N**(-0.5*self.adam_scale) )
-        
+                
         # embed the batch x sequence integers to 
         x = L**( -0.5 * (1-self.adam_scale) )* N**(0.5 * self.adam_scale) * nn.Embed(VOCAB_SIZE, N, embedding_init = kif_first)(x) # batch x seq len x N
-        x = PositionalEncoding(d_model = N)(x)
+        x = N**(0.5 * self.adam_scale) * L**(-0.5 * (1-self.adam_scale) ) * PositionalEncoding(d_model = N, scale = N**(-0.5*self.adam_scale) * L**(0.5 * (1-self.adam_scale)) )(x)
         for l in range(self.depth):
             h = nn.LayerNorm()(x)
             x = x + self.beta/L * Causal_Attention(dim = self.dim, scale_exp = self.scale_exp, heads = self.heads)(nn.gelu(h))
@@ -213,8 +235,9 @@ class Transformer(nn.Module):
             x = x + self.beta/L * MLP_Block(features = N)(nn.gelu(h))
             
         x = nn.LayerNorm()(x)
-        x = L**(-0.5 * (1 - self.adam_scale ) ) * nn.Dense(features = VOCAB_SIZE, use_bias = False, kernel_init = kif_last)(x) / N**(1.0-0.5*self.adam_scale)   # for mean field scaling
+        x = L**(-0.5 * (1 - self.adam_scale ) ) * nn.Dense(features = VOCAB_SIZE, use_bias = True, kernel_init = kif0)(x) / N**(1.0-0.5*self.adam_scale)   # for mean field scaling
         return x
+    
     
 
 def train_model(param_args, opt_args, data = None, adam = False):
@@ -225,11 +248,10 @@ def train_model(param_args, opt_args, data = None, adam = False):
     if adam:
         adam_scale = 1
         schedule = optax.warmup_cosine_decay_schedule(init_value=0.0,peak_value=lr / jnp.sqrt(heads*dim), warmup_steps=100,decay_steps=T,end_value=0.0)
-        #optimizer = optax.adamw( lr / jnp.sqrt(heads*dim) , eps = 1e-20 , weight_decay = 0.0001 )
         optimizer = optax.adamw( schedule , eps = 1e-20 , weight_decay = 0.0 )
     else:
         adam_scale = 0
-        optimizer = optax.sgd( heads * dim * gamma**2 *  lr)
+        optimizer = optax.sgd( heads * dim *  lr)
 
     model = Transformer(dim, heads,depth, scale_exp = scale_exp, adam_scale = adam_scale, beta = beta)
     params = model.init(random.PRNGKey(0), jnp.ones((32,128), dtype = jnp.int32)) 
@@ -244,17 +266,21 @@ def train_model(param_args, opt_args, data = None, adam = False):
     losses = []
 
     for t,batch in enumerate(dataloader):
+        
+        if np.amin(np.array([len(b) for b in batch])) == MAX_LEN:
+        
+            batch = jnp.array(batch)
     
-        loss, grads = val_grad_fn(params, batch[:,:-1], batch[:,1:])
-        updates, opt_state = optimizer.update(grads, opt_state, params)
-        params = optax.apply_updates(params, updates)
-        run_loss = loss
-        #run_loss =  t/(t+1) * run_loss + 1/(t+1) * loss
-        sys.stdout.write(f'\r loss = {run_loss}')
-        wandb.log({'loss': run_loss})
-        losses += [run_loss]
-        if t > T:
-            break
+            loss, grads = val_grad_fn(params, batch[:,:-1], batch[:,1:])
+            updates, opt_state = optimizer.update(grads, opt_state, params)
+            params = optax.apply_updates(params, updates)
+            run_loss = loss
+            #run_loss =  t/(t+1) * run_loss + 1/(t+1) * loss
+            sys.stdout.write(f'\r loss = {run_loss}')
+            wandb.log({'loss': run_loss})
+            losses += [run_loss]
+            if t > T:
+                break
     return losses
 
 
