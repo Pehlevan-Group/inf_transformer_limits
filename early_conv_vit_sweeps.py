add early convergence, VIT LR transfer, fixed Positional encoding scaling in C4

early_conv_vit_sweeps.py
@@ -9,66 +9,63 @@ from jax import random
 from jax.example_libraries import optimizers
 import optax
 import sys
-import wandb
-import argparse
+import seaborn as sns
 import os
+import argparse
 
 
 
 parser = argparse.ArgumentParser(description=''
     '''
-    tests of multiple-layer-per-block models
+    SGD convergence_expt
     ''', formatter_class=argparse.RawTextHelpFormatter)
 
-parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
-parser.add_argument('--mom', default=0.0, type=float, help='momentum')
-parser.add_argument('--batch_size', type=int, default=64)
-parser.add_argument('--arch', type=str, default='simple_TF')
-parser.add_argument('--dataset', type=str,default = 'cifar5m')
-parser.add_argument('--optimizer', default='sgd')
-parser.add_argument('--width', type=int, default = 32)
+parser.add_argument('--lr', default=1.0, type=float, help='learning rate')
+parser.add_argument('--batch_size', type=int, default=128)
+parser.add_argument('--inits', type=int, default=10)
+parser.add_argument('--width', type=int, default = 4)
 parser.add_argument('--heads', type=int, default = 4)
 parser.add_argument('--patch_size',type=int, default=4)
 parser.add_argument('--depth', type=int, default = 2)
-parser.add_argument('--beta', type=float, default=4.0,
+parser.add_argument('--beta', type=float, default= 4.0,
                          help='scaling factor for the residual branch. To use together with res_scaling parameter')
-parser.add_argument('--gamma_zero', type=float, default=0.1,
+parser.add_argument('--gamma_zero', type=float, default=0.2,
                          help='controls the amount of feature learning.')
 parser.add_argument('--scale_exp', type=float, default=1.0)
 parser.add_argument('--steps',type=int, default = 2500)
-parser.add_argument('--save_model', action='store_true')
 
 args = parser.parse_args()
 
-# set sweeps
+
+if args.width == -1:
+    widths = [8, 16, 32,64, 128, 256, 512, 513]
+else:
+    widths = [args.width]
+
 if args.depth == -1:
-    depths = [1,4,16,32]
+    depths = [2,4,8,16,32,64,65]
 else:
     depths = [args.depth]
 
-if args.width == -1:
-    widths = [16,32,64,128,256]
-else:
-    widths = [args.width]
-    
 if args.heads == -1:
-    heads = [ 4,8,16,32,64 ]
+    head_vals = [8, 16, 32, 64, 128, 256, 512, 513]
 else:
-    heads = [args.heads]
+    head_vals = [args.heads]
     
-if args.lr == -1:
-    lrs = np.logspace(-2.5,1.0,12)
-else:
-    lrs = [args.lr]
 
+patch_size = args.patch_size
+scale_exp = args.scale_exp
 beta = args.beta
+gamma = args.gamma_zero
+lr = args.lr
+batch = args.batch_size
+num_inits = args.inits
+T = args.steps
 
-save_dir = '/n/holyscratch01/pehlevan_lab/Users/bbordelon/bbordelon/Learn_gates/cifar_VIT'
 
 
 def get_run_name(args):
-    return "model_{}/dataset_{}/lr_{:.4f}/mom_{:.2f}/batch_size_{}/steps_{}/width_{}/heads_{}/depth_{}/scale_exp_{}/beta_{}/gamma_zero_{}".format(args.arch, args.dataset, args.lr, args.mom, args.batch_size, args.steps, args.width, args.heads, args.depth, args.scale_exp, args.beta, args.gamma_zero)
-
+    return "VIT/early_cifar/lr_{:.4f}/batch_size_{}/steps_{}/width_{}/heads_{}/depth_{}/scale_exp_{}/beta_{}/gamma_zero_{}".format(args.lr, args.batch_size, args.steps, args.width, args.heads, args.depth, args.scale_exp, args.beta, args.gamma_zero)
 
 
 
@@ -111,7 +108,7 @@ class MLP_Block(nn.Module):
         N = self.features
         kif = nn.initializers.normal(stddev = 1.0) # O_N(1) entries
         h = nn.Dense(features = N, kernel_init = kif)(x) / jnp.sqrt(N)
-        h = nn.gelu(h)
+        h = nn.relu(h)
         h = nn.Dense(features = N, kernel_init = kif)(h) / jnp.sqrt(N)
         return h
 
@@ -119,18 +116,58 @@ class MLP_Block(nn.Module):
 class PositionalEncoding(nn.Module):
     d_model : int         # Hidden dimensionality of the input.
     max_len : int  # Maximum length of a sequence to expect.
-    scale: jnp.float32 # scale parameter for initialization
+    kif_scale: jnp.float32
+    scale: jnp.float32
     
     def setup(self):
         # Create matrix of [SeqLen, HiddenDim] representing the positional encoding for max_len inputs
         self.pos_embedding = self.param('pos_embedding', 
-                                        nn.initializers.normal(stddev = self.scale), 
+                                        nn.initializers.normal(stddev=self.scale), 
                                         (1, 1+self.max_len, self.d_model))
 
     def __call__(self, x, train=True):
         B,T,_ = x.shape
-        x = x + self.pos_embedding[:,:T] / self.scale
+        x = x + self.pos_embedding[:,:T]
         return x
+
+class VIT(nn.Module):
+    """A simple VIT model"""
+  
+    dim: int
+    heads: int
+    depth: int
+    patch_size: int
+    scale_exp: jnp.float32
+    adam_scale: int
+
+    @nn.compact
+    def __call__(self, x, train = True):
+        N = self.heads * self.dim
+        D = 3
+        
+        
+        # patchify images
+        x = rearrange(x, 'b (w p1) (h p2) c -> b (w h) (p1 p2 c)', p1 = self.patch_size, p2 = self.patch_size) # (batch, loc, patch_ch_dim )
+        
+        kif_first= nn.initializers.normal(stddev = N**(-0.5*self.adam_scale) ) # O_N(1) entries
+        kif0 = nn.initializers.normal(stddev = 0.0 )
+        kif = nn.initializers.normal(stddev = 1.0) # O_N(1) entries
+
+        x = N**(0.5 * self.adam_scale) * nn.Dense(features = N, kernel_init = kif_first)(x) / jnp.sqrt( 3 * self.patch_size**2 )
+        x = x + PositionalEncoding(d_model = N, max_len = (32 // self.patch_size)**2 )
+        #x = nn.relu(x)
+        for l in range(self.depth):
+            h = nn.LayerNorm()(x)
+            x = x + 5.0/jnp.sqrt(depth) * Attention(dim = self.dim, scale_exp = self.scale_exp, heads = self.heads)(h)
+            h = nn.LayerNorm()(x)
+            x = x + 5.0/jnp.sqrt(depth) * MLP_Block(features = N)(h)
+            
+        # pool over location index
+        x = x.mean(axis = 1) # (batch, N)
+        x = nn.LayerNorm()(x)
+        x = nn.Dense(features = 10, use_bias = False, kernel_init = kif0)(x) / N**(1.0-0.5*self.adam_scale)   # for mean field scaling
+        return x
+    
 
 class simple_TF(nn.Module):
     
@@ -170,56 +207,8 @@ class simple_TF(nn.Module):
         return x
 
 
-class VIT(nn.Module):
     
-    # simple VIT model with
-    dim: int
-    heads: int
-    depth: int
-    patch_size: int
-    scale_exp: jnp.float32 = 1.0
-    adam_scale: int = 0.0
-    beta: jnp.float32 = 4.0
     
-    @nn.compact
-    def __call__(self, x, train = True):
-        N = self.heads * self.dim
-        L = self.depth
-        D = 3
-        
-        # patchify images
-        x = rearrange(x, 'b (w p1) (h p2) c -> b (w h) (p1 p2 c)', p1 = self.patch_size, p2 = self.patch_size) # (batch, loc, patch_ch_dim )
-        
-        kif_first= nn.initializers.normal(stddev = N**(-0.5*self.adam_scale) * L**(0.5 * (1.0-self.adam_scale)) ) # O_N(1) entries
-        kif = nn.initializers.normal( stddev = 1.0 ) # O_N(1) entries
-        kif_last = nn.initializers.normal(stddev = L**(0.5 * (1-self.adam_scale) ) )
-        
-        # read-in weights
-        x = L**(-0.5 * (1.0-self.adam_scale)) * N**(0.5 * self.adam_scale) * nn.Dense(features = N, kernel_init = kif_first)(x) / jnp.sqrt( D * self.patch_size**2 )
-        # positional encoding
-        x = PositionalEncoding(d_model = N, max_len = (32//self.patch_size)**2, scale = N**(-0.5*self.adam_scale) * L**(0.5 * (1.0-self.adam_scale)))
-        # residual stream with pre-LN
-        for l in range(self.depth):
-            h = nn.LayerNorm()(x)
-            h = Attention(dim = self.dim, scale_exp = self.scale_exp, heads = self.heads)( h )
-            x = x + self.beta / L * h
-            h = nn.LayerNorm()(x)
-            h = nn.Dense(features = N, kernel_init = kif)( h ) / jnp.sqrt(N)
-            h = nn.gelu(h)
-            h = nn.Dense(features = N, kernel_init = kif)( h ) / jnp.sqrt(N)
-            x = x + self.beta / L * h
-        
-        # last norm layer
-        x = nn.LayerNorm()(x)
-        # pool over spatial dimension
-        x = x.mean(axis = 1) # (batch, N)
-        #x = rearrange(x, 'b l d -> b (l d)')
-        x = L**(-0.5*(1-self.adam_scale)) * nn.Dense(features = 10, use_bias = False, kernel_init = kif_last)(x) / N**(1.0-0.5*self.adam_scale)   # for mean field scaling
-        return x
-
-
-    
-
 data_dir = '/n/holyscratch01/pehlevan_lab/Everyone/cifar-5m-new'
 file_name = f"{data_dir}/cifar5m_part{0}.npz"
 part0 = np.load(file_name, allow_pickle=True)
@@ -233,6 +222,8 @@ std = X.std()
 X = (X - mean) / std
 print(y.shape)
 print(X.shape)
+print(X.dtype)
+
 
 
 def get_data(dset_count):
@@ -249,7 +240,7 @@ def get_data(dset_count):
     X = (X - mean) / std
     return X, y
 
-def train_model(param_args, opt_args, data = None, adam = False):
+def train_model(param_args, opt_args, data = None, adam = False, seed = 0):
 
     dim, heads, depth, patch_size, scale_exp, beta = param_args
     T, batch, gamma, lr = opt_args
@@ -260,19 +251,14 @@ def train_model(param_args, opt_args, data = None, adam = False):
 
     else:
         adam_scale = 0.0
-        opt_init, opt_update, get_params = optimizers.sgd( heads * dim * gamma**2 * depth *  lr)
+        opt_init, opt_update, get_params = optimizers.sgd( depth * heads * dim * gamma**2 *  lr)
 
-    if args.arch == "vit":
-        model = VIT(dim = dim, heads = heads, depth = depth, patch_size = patch_size, scale_exp = scale_exp, adam_scale = adam_scale, beta = beta)
-    else:
-        model = simple_TF(dim = dim, heads = heads, depth = depth, patch_size = patch_size, scale_exp = scale_exp, adam_scale = adam_scale, beta = beta)
-
-    
-    params = model.init(random.PRNGKey(0), jnp.ones((4,32,32,3)) )['params']                                                               
+    model = simple_TF(dim = dim, heads = heads, depth = depth, patch_size = patch_size, scale_exp = scale_exp, adam_scale = adam_scale, beta = beta)
+    params = model.init(random.PRNGKey(seed), jnp.ones((4,32,32,3)) )['params']
+                                                               
     opt_state = opt_init(params)
-    
+
     shift_fn = jax.jit(lambda p, x: (model.apply({'params':p}, x) - model.apply({'params':params}, x)) / gamma)
-    #shift_fn = jax.jit(lambda p, x: model.apply({'params':p}, x) / gamma)
     loss_fn = jax.jit(lambda params, Xb, yb: optax.softmax_cross_entropy_with_integer_labels(logits=shift_fn(params, Xb), labels=yb).mean())
     grad_fn = jax.jit(jax.grad(loss_fn))
 
@@ -281,11 +267,12 @@ def train_model(param_args, opt_args, data = None, adam = False):
     
     loss_t = 0.0
     dset_count = 0
-    comp_every = 25
     
     if data != None:
         X,y = data
     
+    comp_every = 10
+    loss_t = 0.0
     for t in range(T):
     
         if data == None:
@@ -308,41 +295,39 @@ def train_model(param_args, opt_args, data = None, adam = False):
         
         if t % comp_every == 0 and t > 0:
             losses += [loss_t]
-            wandb.log({'loss': loss_t })
             sys.stdout.write(f'\r loss = {loss_t}')
             loss_t = 0.0
 
         loss_t += 1/comp_every * loss_fn(get_params(opt_state), Xt, yt)
-            
-        opt_state = opt_update(t,grad_fn(get_params(opt_state), Xt, yt),opt_state)
+        opt_state = opt_update(t, grad_fn(get_params(opt_state), Xt, yt),opt_state)
+        
+    preds = shift_fn(get_params(opt_state), Xt)
+    return losses, preds
+
+save_dir = '/n/holyscratch01/pehlevan_lab/Users/bbordelon/bbordelon/Learn_gates/early_conv'
+
+opt_args = ( T, batch, gamma, lr )
+
+for i, width in enumerate(widths):
+    for j,heads in enumerate(head_vals):
+        for k,depth in enumerate(depths):
     
-    return losses
-
-
-# sweep over width, depth, gates, and lrs
-for i, dim in enumerate(widths):
-    for j, depth in enumerate(depths):
-        for k, head in enumerate(heads):
-            for l, lr in enumerate(lrs):
-            
-                args.width = dim
-                args.depth = depth
-                args.lr = lr
-                args.heads = head
-                
-                run_name = get_run_name(args)
-                
-                    
-                opt_args = ( args.steps , args.batch_size, args.gamma_zero, args.lr )
-                param_args = (dim, head, depth, args.patch_size, args.scale_exp , args.beta )
-
-                wandb.init(
-                    project="scaling_NL",
-                    # track hyperparameters and run metadata
-                    config=args.__dict__)
-                
-                wandb.run.name = run_name
-                losses = train_model(param_args, opt_args, data = None)
-                save_path = os.path.join(save_dir, run_name.replace("/", "-"))
-                np.save(save_path, losses)
-                wandb.finish()
+            all_losses_ijk = []
+            all_preds_ijk = []
+            args.width = width
+            args.heads = heads
+            args.depth = depth
+            print(f"width = {width}, heads = {heads} , depth = {depth}")
+            for j in range(num_inits):
+                param_args = (width, heads, depth, patch_size, scale_exp, beta)    
+                losses, preds = train_model(param_args, opt_args, data = (X,y), seed = j)
+        
+                all_losses_ijk += [losses] 
+                all_preds_ijk += [preds]
+        
+    
+            run_name = get_run_name(args)
+            save_path = os.path.join(save_dir, run_name.replace("/", "-") + '-losses.npy')
+            np.save(save_path, jnp.array(all_losses_ijk))
+            save_path = os.path.join(save_dir, run_name.replace("/", "-") + '-preds.npy')
+            np.save(save_path, jnp.array(all_preds_ijk))

train_C4.py
@@ -203,7 +203,7 @@ class PositionalEncoding(nn.Module):
 
     def __call__(self, x, train=True):
         B,T,_ = x.shape
-        x = x + self.pos_embedding[:,:T]
+        x = x + self.pos_embedding[:,:T] / self.scale
         return x
 
 class Transformer(nn.Module):
@@ -227,7 +227,7 @@ class Transformer(nn.Module):
                 
         # embed the batch x sequence integers to 
         x = L**( -0.5 * (1-self.adam_scale) )* N**(0.5 * self.adam_scale) * nn.Embed(VOCAB_SIZE, N, embedding_init = kif_first)(x) # batch x seq len x N
-        x = N**(0.5 * self.adam_scale) * L**(-0.5 * (1-self.adam_scale) ) * PositionalEncoding(d_model = N, scale = N**(-0.5*self.adam_scale) * L**(0.5 * (1-self.adam_scale)) )(x)
+        x = PositionalEncoding(d_model = N, scale = N**(-0.5*self.adam_scale) * L**(0.5 * (1-self.adam_scale)) )(x)
         for l in range(self.depth):
             h = nn.LayerNorm()(x)
             x = x + self.beta/L * Causal_Attention(dim = self.dim, scale_exp = self.scale_exp, heads = self.heads)(nn.gelu(h))